dbaecab9767d2cb04e64214c3601ee383d23b44b
Boulet-Clement <clement.boulet60510@gmail.com>
2021-11-03 14:57:50 +0100

    modification de routes.php

--- config/routes.php
+++ config/routes.php
@@ -6,13 +6,9 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
 
 
-<<<<<<< HEAD
-$app->get('/', \App\Controller\UserController::class . ':test');
-=======
 $app->get('/',function ($request, $response, array $args){
-    return $this->get(Twig::class)->render($response,"home/index.html.twig");
+    return $this->get(Twig::class)->render($response,"home/accueil.html.twig");
 });
 
 $app->get('/test', HomeAction::class);
 
->>>>>>> 3bbfcc898be19e15bbf468b85d30c384f040848f
