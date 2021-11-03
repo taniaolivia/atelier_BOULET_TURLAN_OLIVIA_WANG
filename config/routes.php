0abcb09e3e204530d94ab3143407f86bdfc8be68
Boulet-Clement <clement.boulet60510@gmail.com>
2021-11-03 11:23:48 +0100

    Un début de vue

--- config/routes.php
+++ config/routes.php
@@ -2,9 +2,9 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+$app->get('/',function ($request, $response, array $args){
+    return $this->get(Twig::class)->render($response,"home/index.html.twig",["session"=>$_SESSION]);
 });
