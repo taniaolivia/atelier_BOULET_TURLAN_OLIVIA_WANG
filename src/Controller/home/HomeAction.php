67a8f14bc6681e475f7c32017823846c65766a58
Tania Olivia <taniaolivia9@gmail.com>
2021-11-03 15:35:07 +0100

    modification de home

--- src/Controller/home/HomeAction.php
+++ src/Controller/home/HomeAction.php
@@ -3,16 +3,21 @@ namespace App\Controller\home;
 
 use App\Controller\ActionController;
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class HomeAction extends ActionController{
 
-    protected function action():Response
-    {        
-        $this->response->getBody()->write("Hello");
-        return $this->response;
-    }
-    public function __construct()
+    public $container;
+
+    public function __construct(ContainerInterface $container)
     {
+        $this->container = $container;
     }
+
+    protected function action():Response
+    {
+        return $this->container->get('view')->render($this->response, 'accueil/accueil.html.twig');
+    }
+
 }
