39a995ebb48a81362c6ea7fd141b5e936d302499
Tania Olivia <taniaolivia9@gmail.com>
2021-11-03 15:34:23 +0100

    modification de route

--- config/routes.php
+++ config/routes.php
@@ -4,11 +4,10 @@ use App\Controller\home\HomeAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Views\Twig;
+use DI\ContainerBuilder;
 
 
-$app->get('/',function ($request, $response, array $args){
-    return $this->get(Twig::class)->render($response,"accueil/accueil.html.twig");
-});
+$app->get('/', HomeAction::class);
 
-$app->get('/test', HomeAction::class);
+
 
