e745023fc529c28de602c4794b475588eec3e815
Boulet-Clement <clement.boulet60510@gmail.com>
2021-11-04 11:41:42 +0100

    Création de groupes pour les routes

--- config/routes.php
+++ config/routes.php
@@ -1,4 +1,5 @@
 <?php
+use Slim\Routing\RouteCollectorProxy;
 
 use App\Controller\Accueil\AccueilAction;
 use App\Controller\Produit\ListeProduitsAction;
@@ -8,10 +9,13 @@ use App\Controller\Utilisateurs\Producteur\DetailProducteurAction;
 use App\Controller\Utilisateurs\Gerant\ListCommandesAction;
 
 $app->get('/', AccueilAction::class);
-$app->get('/list-commandes', ListCommandesAction::class);
 $app->get('/nosproduits', ListeProduitsAction::class)->setName('nosproduits');
 $app->get('/nosproduits/{produit}', DetailProduitAction::class)->setName('detailproduit');
 $app->get('/nosproducteurs', ListeProducteursAction::class)->setName('nosproducteurs');
 $app->get('/nosproducteurs/{producteur}', DetailProducteurAction::class)->setName('detailproducteur');
 
+$app->group('/gerant', function (RouteCollectorProxy $group) {
+
+    $group->get('/list-commandes', ListCommandesAction::class);
+});
 
---
+++ src/Controller/Utilisateurs/Gerant/ViewDetailCommandAction.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Controller\Utilisateurs\Gerant;
+
+use App\Controller\ActionController;
+use App\Repository\CommandeRepository;
+use Psr\Container\ContainerInterface;
+use Doctrine\ORM\EntityManager;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ListCommandesAction extends ActionController{
+
+    public $container;
+    private $commandeRepository;
+
+    public function __construct(ContainerInterface $container,CommandeRepository $commandeRepository)
+    {
+        $this->container = $container;
+        $this->commandeRepository = $commandeRepository;
+    }
+
+    protected function action():Response
+    {
+        $commandes = $this->commandeRepository->findAllCommandesWithClientName();
+        return $this->container->get('view')->render($this->response, 'commande/listCommandes.html.twig',[
+            "commandes"=>$commandes
+        ]);                
+    }
+
+}
