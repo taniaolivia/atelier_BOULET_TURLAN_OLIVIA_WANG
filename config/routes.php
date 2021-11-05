6973e562113071e38380085a5bca7d7de7e827b8
Boulet-Clement <clement.boulet60510@gmail.com>
2021-11-05 10:59:06 +0100

    Ajout / Suppression ok

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,9 @@ use App\Controller\Utilisateurs\Producteur\DetailProducteurAction;
 use App\Controller\Utilisateurs\Gerant\ListCommandesAction;
 use App\Controller\Utilisateurs\Gerant\ViewDetailCommandAction;
 
+use App\Controller\Panier\AjoutPanierAction;
+use App\Controller\Panier\SuppressionPanierAction;
+
 $app->get('/', AccueilAction::class);
 $app->get('/nosproduits', ListeProduitsAction::class)->setName('nosproduits');
 $app->get('/nosproduits/{produit}', DetailProduitAction::class)->setName('detailproduit');
@@ -29,5 +32,15 @@ $app->group('/gerant', function (RouteCollectorProxy $group) {
 
     $group->get('/list-commandes', ListCommandesAction::class);
     $group->get('/commande/{id}', ViewDetailCommandAction::class);
+});
+$app->group('/panier', function (RouteCollectorProxy $group) {
+    $group->get('', function($request, $response, $args){
+        return $this->get('view')->render($response, 'panier/panier.html.twig',[
+            "session"=> $_SESSION
+        ]);
+    });
+    $group->post('/ajout/{id}', AjoutPanierAction::class);
+    $group->post('/suppression/{id}', SuppressionPanierAction::class);
+    //$group->get('/retrait/{id}', ViewDetailCommandAction::class);
 });
 
